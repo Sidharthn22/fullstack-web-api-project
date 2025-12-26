94dd64e237f871b22cc8fe7c32a3f912c9edab42
Sidharth N <sidharthn22102003@gmail.com>
2025-12-26 10:58:53 +0530

    Fix routing for GitHub Pages using HashRouter

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { Loginpage } from "./Loginpage";
 import Userportal from "./components/user/Userportal";
 import Adminportal from "./components/admin/Adminportal";
 
-
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Loginpage/>}/>
-        <Route path="/userportal/*" element={<Userportal/>} />
-        <Route path="/adminportal/*" element={<Adminportal/>} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Loginpage />} />
+      <Route path="/userportal/*" element={<Userportal />} />
+      <Route path="/adminportal/*" element={<Adminportal />} />
+    </Routes>
   );
 }
 
